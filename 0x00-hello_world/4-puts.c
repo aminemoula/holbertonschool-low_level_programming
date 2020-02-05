eb36e373cf444ffc8ca6d8e9b705c4fb73927073
aminemoula <moulaamine2018@gmail.com>
2020-02-05 14:36:17 +0100

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * 
- * Return: 0 if success.
+ * Return: 0 true
  */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle,\n");
 	return 0;
 }
