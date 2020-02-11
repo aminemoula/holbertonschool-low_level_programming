957f5481033b96141fc9a92e357d32628da168da
aminemoula <moulaamine2018@gmail.com>
2020-02-11 13:56:43 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @x : number
+ * Return: Always 0.
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (x * -1);
+}
