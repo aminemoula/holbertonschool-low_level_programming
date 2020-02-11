7ad703b0f64593ba50aa50a0ab7aef6214135455
aminemoula <moulaamine2018@gmail.com>
2020-02-11 11:25:46 +0100

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,16 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i;
-for (i = 'a'; i <= 'z'; i<=10; i++)
+int x;
+for (x = 0 ; x <= 10 ; i++)
+{
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
+}
 }
