32d7093467eb4c96f9d23e75f496d120e8ca2fc6
aminemoula <moulaamine2018@gmail.com>
2020-02-11 14:45:26 +0100

    fix7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,14 @@
  */
 int print_last_digit(int n)
 {
-if (n >= 0)
-{
-_putchar(n % 10 + '0');
-return (n % 10);
-}
-else
+if (n < 0)
 {
 _putchar((n % 10) * -1 + '0');
 return ((n % 10) * -1);
+}
+else
+{
+_putchar(n % 10 + '0');
+return (n % 10);
 }
 }
