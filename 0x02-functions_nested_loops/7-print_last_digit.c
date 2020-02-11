2dd00c01f6e1181fbba5f90ea278e38181a695c7
aminemoula <moulaamine2018@gmail.com>
2020-02-11 14:12:51 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @n : type
+ * Return: Always 0.
+ */
+ int print_last_digit(int n);
+ 
+ int l;
+
+ 	if (n >= 0)
+ 		l = n % 10;
+ 	else
+ 		l = (n % 10) * -1;
+ 	_putchar(l + '0');
+ 	return (l);
+ }
