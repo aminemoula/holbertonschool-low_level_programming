3df75815a7919043808694a50718467653d3e654
aminemoula <moulaamine2018@gmail.com>
2020-02-11 14:50:52 +0100

    fix7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_last_digit - check the code for Holberton School students.
- *  @n : char to check
+ * @n : char to check
  * Return: Always 0.
  */
 int print_last_digit(int n)
