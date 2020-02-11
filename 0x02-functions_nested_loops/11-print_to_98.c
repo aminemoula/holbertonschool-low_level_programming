bd740c5fe8a9d8789a63cdc6ee785384678ee203
aminemoula <moulaamine2018@gmail.com>
2020-02-11 18:35:54 +0100

    print 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+/**
+ * print_below_98 - check numbers
+ * @a: int type
+ *
+ *
+ * Return: return values
+ */
+void print_below_98(int a)
+{
+	int b;
+
+	for (b = a; b <= 98; b++)
+	{
+		if (b != 98)
+			printf("%d, ", b);
+		else
+			printf("%d\n", b);
+	}
+}
+
+/**
+ * print_above_98 - check numbers
+ * @d: int type
+ *
+ *
+ * Return: return values
+ */
+
+void print_above_98(int d)
+{
+	int b;
+
+	for (b = d;  b >= 98; b--)
+	{
+		if (b != 98)
+			printf("%d, ", b);
+		else
+			printf("%d\n", b);
+	}
+}
+
+/**
+ * print_to_98 - check numbers
+ * @n: int type
+ *
+ *
+ * Return: return values
+ */
+
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+		print_below_98(n);
+	else
+		print_above_98(n);
+}
