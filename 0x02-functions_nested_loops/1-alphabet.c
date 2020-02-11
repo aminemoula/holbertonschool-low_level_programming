375534b5eab84224a95d27b9b737a0eafb98d95e
aminemoula <moulaamine2018@gmail.com>
2020-02-11 10:38:22 +0100

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *   print_alphabet - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+
+int print_alphabet(void)
+{
+	char c;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
