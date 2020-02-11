cadd98cdb82ecc9c12a79d3c988bc747461385c3
aminemoula <moulaamine2018@gmail.com>
2020-02-11 14:32:51 +0100

    fix7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,11 @@ int print_last_digit(int n);
 if (n >= 0)
 {
 _putchar (n % 10 + '0');
-return(n % 10);
+return (n % 10);
 }
+{
 else
 _putchar((n % 10) * -1 + '0');
+}
 return ((n % 10) * -1);
 }
