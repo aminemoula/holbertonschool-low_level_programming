d2af507a03767975d59a17f427ae1120cdc99415
aminemoula <moulaamine2018@gmail.com>
2020-02-13 13:26:52 +0100

    more functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 /**
-* print_numbers - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
+ * print_numbers - check the code for Holberton School students.
+ * Return: Always 0.
+ */
 
 void print_numbers(void)
 {
