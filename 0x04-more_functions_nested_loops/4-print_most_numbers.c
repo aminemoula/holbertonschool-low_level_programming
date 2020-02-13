b47b5d8b3aabcbc7ed9d9e007ba2e4b6fccb45b3
aminemoula <moulaamine2018@gmail.com>
2020-02-13 14:40:37 +0100

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include <holberton.h>
+#include "holberton.h"
 
 /**
  * print_most_numbers - check the code for Holberton School students.
@@ -7,7 +7,7 @@
 void print_most_numbers(void)
 {
 int i;
-for (i = 0; i < 10 ; i++)
+for (i = 0; i < 10; i++)
 {
 if (i != 2 && i != 4)
 {
