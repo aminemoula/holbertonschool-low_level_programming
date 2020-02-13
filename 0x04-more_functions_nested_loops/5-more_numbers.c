b01b966bd0dcc04dd5d4f80e61adb9fa4914be2c
aminemoula <moulaamine2018@gmail.com>
2020-02-13 21:21:49 +0100

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,19 +4,19 @@
 *
 * Return: Always 0.
 */
-void more_numbers(void);
+void more_numbers(void)
 {
 int i, x;
-for (x = 0 ; x <= 9 ; x++)
+for (x = 0; x < 10 ; x++)
 {
-for (i = 0, i <= 14, i++)
+for (i = 0; i < 15 ; i++)
 {
 if (i > 9)
 {
-_putchar (i / 10 + '0');
+_putchar(i / 10 + '0');
 }
 _putchar(i % 10 + '0');
 }
-_putchar ('\n');
+_putchar('\n');
 }
 }
