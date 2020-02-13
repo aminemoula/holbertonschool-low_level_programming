2c04f54d55a43475858eefb0664b6ceb50521d43
aminemoula <moulaamine2018@gmail.com>
2020-02-13 22:33:32 +0100

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_square - print suqres
+* @size: number
+*
+* Return: prints hashes in squares
+*/
+
+void print_square(int size)
+{
+int a;
+for (a = 1; a <= size; a++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (size < 1)
+_putchar('\n');
+}
