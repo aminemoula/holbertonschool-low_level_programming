bfc3c4b73744b0e4db3931aa13990a02586b7308
aminemoula <moulaamine2018@gmail.com>
2020-02-13 14:36:07 +0100

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include <holberton.h>
+
 /**
  * print_most_numbers - check the code for Holberton School students.
  * Return: Always 0.
  */
-
 void print_most_numbers(void)
 {
 int i;
