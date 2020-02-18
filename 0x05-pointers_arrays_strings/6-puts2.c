da337e331d4c61e4ec2f8f31abf910f9e2bc5ec8
aminemoula <moulaamine2018@gmail.com>
2020-02-18 17:29:17 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print one char out of 2 
+ * @str: char array string 
+ * 
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
