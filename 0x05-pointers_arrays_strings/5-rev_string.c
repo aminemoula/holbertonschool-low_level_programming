e37f36ecb0175edbe009872642e447e4e8e6a890
aminemoula <moulaamine2018@gmail.com>
2020-02-18 14:55:31 +0100

    fix

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * rev_string - reverse a string
+ * @s : char
+ */
+void rev_string(char *s)
+{
+int i, j;
+char r[10];
+i = j = 0;
+while (s[i] != '\0')
+{
+r[i] = s[i];
+i++;
+}
+i--;
+while (i >= 0)
+{
+s[i] = r[j];
+i--;
+j++;
+}
+}
