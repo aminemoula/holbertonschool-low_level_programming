aca64563d0e5f5485604c7b90e541534d6df15b5
aminemoula <moulaamine2018@gmail.com>
2020-02-19 09:49:33 +0100

    fix9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,16 +7,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
+int i = 0, j;
 while (src[i] != '\0')
 {
-dest[i] = src[i];
 i++;
 }
+for (j = 0; j <= i; j++)
+{
+dest[j] = src[j];
+}
 return (dest);
-
-
-
-
-
 }
