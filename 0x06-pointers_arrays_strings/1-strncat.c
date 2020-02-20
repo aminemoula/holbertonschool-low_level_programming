ecab7548ca6ed1e40802e1c9a5a6f6e670a1a26f
aminemoula <moulaamine2018@gmail.com>
2020-02-20 18:27:44 +0100

    more PAS

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+/**
+ * _strncat - two strings
+ * @dest: pointer
+ * @src: pointer
+ * @n: number
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	return (dest);
+}
